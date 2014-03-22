08a79d3f379815010ac665862ad41f46fd0e8077
ashishtamrakar <ashishtamrakar@lftechnology.com>
2014-03-22 13:00:38 +0545

    Adds the new folder

--- demos/blog/protected/config/params.php
+++ demos/blog/protected/config/params.php
@@ -5,7 +5,7 @@ return array(
 	// this is displayed in the header section
 	'title'=>'My Yii Blog',
 	// this is used in error pages
-	'adminEmail'=>'webmaster@example.com',
+	'adminEmail'=>'ashishtamrakar@gmail.com',
 	// number of posts displayed per page
 	'postsPerPage'=>10,
 	// maximum number of comments that can be displayed in recent comments portlet
@@ -15,5 +15,5 @@ return array(
 	// whether post comments need to be approved before published
 	'commentNeedApproval'=>true,
 	// the copyright information displayed in the footer section
-	'copyrightInfo'=>'Copyright &copy; 2009 by My Company.',
+	'copyrightInfo'=>'Copyright &copy; 2014 by Ashish Tamrakar.',
 );
--- demos/leapfrogems/protected/config/main.php
+++ demos/leapfrogems/protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'Learning Blog Yii',
+	'name'=>'My Web Application',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
@@ -18,7 +18,17 @@ return array(
 		'application.components.*',
 	),
 
-	'defaultController'=>'post',
+	'modules'=>array(
+		// uncomment the following to enable the Gii tool
+		/*
+		'gii'=>array(
+			'class'=>'system.gii.GiiModule',
+			'password'=>'Enter Your Password Here',
+			// If removed, Gii defaults to localhost only. Edit carefully to taste.
+			'ipFilters'=>array('127.0.0.1','::1'),
+		),
+		*/
+	),
 
 	// application components
 	'components'=>array(
@@ -26,30 +36,33 @@ return array(
 			// enable cookie-based authentication
 			'allowAutoLogin'=>true,
 		),
-		// 'db'=>array(
-		// 	'connectionString' => 'sqlite:protected/data/blog.db',
-		// 	'tablePrefix' => 'tbl_',
-		// ),
-		// uncomment the following to use a MySQL database
+		// uncomment the following to enable URLs in path-format
+		/*
+		'urlManager'=>array(
+			'urlFormat'=>'path',
+			'rules'=>array(
+				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
+				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
+				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
+			),
+		),
+		*/
 		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=learning_yii_test',
+			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
+		),
+		// uncomment the following to use a MySQL database
+		/*
+		'db'=>array(
+			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
 			'emulatePrepare' => true,
 			'username' => 'root',
 			'password' => '',
 			'charset' => 'utf8',
-			'tablePrefix' => 'tbl_',
 		),
+		*/
 		'errorHandler'=>array(
 			// use 'site/error' action to display errors
 			'errorAction'=>'site/error',
-		),
-		'urlManager'=>array(
-			'urlFormat'=>'path',
-			'rules'=>array(
-				'post/<id:\d+>/<title:.*?>'=>'post/view',
-				'posts/<tag:.*?>'=>'post/index',
-				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
-			),
 		),
 		'log'=>array(
 			'class'=>'CLogRouter',
@@ -59,18 +72,19 @@ return array(
 					'levels'=>'error, warning',
 				),
 				// uncomment the following to show log messages on web pages
-				
-				// array(
-				// 	'class'=>'CWebLogRoute',
-				// ),
-				
+				/*
+				array(
+					'class'=>'CWebLogRoute',
+				),
+				*/
 			),
 		),
 	),
 
 	// application-level parameters that can be accessed
 	// using Yii::app()->params['paramName']
-	'params'=>require(dirname(__FILE__).'/params.php'),
-
-	'timeZone' => "Asia/Kathmandu",
+	'params'=>array(
+		// this is used in contact page
+		'adminEmail'=>'webmaster@example.com',
+	),
 );
